15e8479c01b4c9356ad2bf1f954d7b9956e67b76
dev-wantap <gwk1125@hanyang.ac.kr>
2021-10-13 16:52:31 +0900

    Update Assignment6.py

--- Assignment6.py
+++ Assignment6.py
@@ -1,11 +1,15 @@
 # 컴퓨팅과데이터처리 6주차 과제 
 # 2021024202 김관우
 
+guguLine = ""
+
 for i in range(2, 10):
-    print(f"#  {i}단  #", end=" ")
-print()
+    guguLine += f"#  {i}단  # "
+print(guguLine)
+
 for i in range(1, 10):
+    guguLine = ""
     for j in range (2, 10):
-        print(str(j)+"*"+str(i)+" = %2d"%(i*j),  end="  ")
-    print()
+        guguLine += (str(j)+"*"+str(i)+" = %2d  "%(i*j))
+    print(guguLine)
 
